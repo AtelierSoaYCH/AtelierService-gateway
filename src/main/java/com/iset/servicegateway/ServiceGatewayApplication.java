8e0abeea04aaa5530b886316a6f6f16cce481d6e
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-19 13:39:33 +0100

    add routes bean

--- src/main/java/com/iset/servicegateway/ServiceGatewayApplication.java
+++ src/main/java/com/iset/servicegateway/ServiceGatewayApplication.java
@@ -2,12 +2,23 @@ package com.iset.servicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceGatewayApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/societes/**")
+						.uri("http://localhost:8081"))
+				.build();
 	}
 
 }
